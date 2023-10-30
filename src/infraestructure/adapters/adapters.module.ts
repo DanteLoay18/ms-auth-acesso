4a9e4f8d45dc44c8a97e742f9a2bcada6126f299
Dante Loayza <danteloayza8@gmail.com>
2023-10-30 14:13:56 -0500

    Se cambio la hora de expiracion a 4h

--- src/infraestructure/adapters/adapters.module.ts
+++ src/infraestructure/adapters/adapters.module.ts
@@ -30,7 +30,7 @@ const providers = [
                 return {
                     secret: configService.get('JWT_SECRET'),
                     signOptions:{
-                        expiresIn:'2h'
+                        expiresIn:'4h'
                     }
                 }
             }
